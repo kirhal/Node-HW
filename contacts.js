c51b7a075ec61f2973e5ac33418fb3e05c558824
kirhal <kirillgaleckiy@gmail.com>
2023-06-17 17:03:31 +0200

    upd

---
+++ contacts.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, './db/contacts.json');
+
+console.log(contactsPath);
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
